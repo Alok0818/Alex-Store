8c7acdf48bf4dde4a18432f288136584b2871227
Alok Kushwaha <94324872+Alok0818@users.noreply.github.com>
2022-06-17 00:56:12 +0530

    home page created

---
+++ src/components/Home.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { CartState } from "../context/Context";
+import SingleProduct from "./SingleProduct";
+import "./style.css";
+import Filters from "./Filters";
+const Home = () => {
+  const {
+    state: { products },
+    productState: { byStock, byFastDelivery, sort, byRating, searchQuery },
+  } = CartState();
+
+  console.log(products);
+
+  const transformProducts = () => {
+    let sortedProducts = products;
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? (a.price - b.price) : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.byFastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
+
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {transformProducts().map((prod) => (
+          <SingleProduct product={prod} key={prod.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
